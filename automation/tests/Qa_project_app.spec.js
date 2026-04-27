19c59623837bdae6309bb6894796895c2e8cea2d
auyber <auybergm@hotmail.com>
2026-04-27 11:59:12 -0300

    fix: correct syntax error in browser login validation test

--- automation/tests/Qa_project_app.spec.js
+++ automation/tests/Qa_project_app.spec.js
@@ -4,5 +4,11 @@ import { test, expect } from '@playwright/test';
 test('Browser login validation', async ({ page }) => {
    await page.goto('http://localhost:5173/')
    await expect(page).toHaveURL('http://localhost:5173/');
-   await 
+   await page.locator('#UserName').fill('usertest@gmail.com');
+   await page.locator('#Password').fill('user123@#');
+   await page.locator('button[type="submit"]').click();
+   await page.locator('#root h1').waitFor();
+   const title = await page.locator('#root h1').textContent();
+   console.log(title);
+
 });
